4bd269bd49b091c40c92919fff723b73d049ec69
sreeharikmarar <sreeharikmarar@gmail.com>
2013-05-13 18:18:25 +0530

    git hub model has been pushed

--- app/models/github_details.rb
+++ app/models/github_details.rb
@@ -11,32 +11,24 @@ class GithubDetails < ActiveRecord::Base
 
   
   ## Specifies a white list of attributes that can be set via mass-assignment.
-  attr_accessible :user_id, :project_name , :project_description ,:public_url , :technologies , :create_at , :updated_at , :contribution
+  attr_accessible :user_id, :project_name , :project_description ,:public_url , :technologies , :created_at , :updated_at , :contribution
 
 
   def self.parse_github(user, datas)
     user.github_details.destroy_all if user.github_details.any?
-    puts "*"*100
+    if datas.length > 0
       datas.each do |item|
-       puts "-"*100
-      @github_detail = GithubDetails.new
-      @github_detail.user_id = user.id
-      puts "@github_detail.user_id : #{@github_detail.user_id}"
-      @github_detail.project_name = item["name"]
-      puts "@github_detail.project_name  : #{@github_detail.project_name}"
-      @github_detail.project_description = item["description"]
-      puts "@github_detail.project_description  : #{@github_detail.project_description}"
-      @github_detail.public_url = item["html_url"]
-      puts "@github_detail.public_url  : #{@github_detail.public_url}"
-      @github_detail.technologies = item["language"]
-      puts " @github_detail.technologies  : #{@github_detail.technologies}"
-      @github_detail.created_at = item["created_at"]
-      puts " @github_detail.created_at : #{ @github_detail.created_at}"
-      @github_detail.updated_at = item["updated_at"]
-      puts " @github_detail.updated_at : #{@github_detail.updated_at}"
-      @github_detail.contribution = ''
-      @github_detail.save
-       puts "="*100
+        @github_detail = GithubDetails.new
+        @github_detail.user_id = user.id
+        @github_detail.project_name = item["name"]
+        @github_detail.project_description = item["description"]
+        @github_detail.public_url = item["html_url"]
+        @github_detail.technologies = item["language"]
+        @github_detail.created_at = item["created_at"]
+        @github_detail.updated_at = item["updated_at"]
+        @github_detail.contribution = ''
+        @github_detail.save
+      end
     end
   end
 end
