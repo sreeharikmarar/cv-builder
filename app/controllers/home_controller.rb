22c8c5a0290507fe90b97159f0c5ee86c9806167
sreeharikmarar <sreeharikmarar@gmail.com>
2013-11-10 21:00:56 +0530

    about page has been created

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,11 @@
 class HomeController < ApplicationController
-  before_filter :redirect_to_home_if_signed_in
+  
+  before_filter :redirect_to_home_if_signed_in , :only => :index
+
   def index
     
   end
+
   def about
     
   end
